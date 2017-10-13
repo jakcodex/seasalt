9cca049009a849a9ddcf7e5794692149aecc1786
Jakcodex <28816370+jakcodex@users.noreply.github.com>
2017-10-13 04:34:54 -0500

    v0.0.2

--- README.md
+++ README.md
@@ -6,21 +6,52 @@ See it in action here: <a href="https://jakcodex.github.io/seasalt/seasalt.html"
 
 ## Example
 
+#### AEAD Encryption
+
+Utilizes the XCHACHA20-POLY1305-IETF 2-way Cipher
+
+```js
+let seasalt = new SeaSalt({
+	passphrase: 'a3a44974a290878ad341befe2b96dc561d7692e02ab39c7bef26c67f37a5f46e'
+});
+let ciphertext = seasalt.encrypt('test');
+let decrypted = seasalt.decrypt(ciphertext);
+
+// or
+
+let seasalt = new SeaSalt;
+let original = 'test';
+let ciphertext = seasalt.encrypt(
+	original, 
+	'a3a44974a290878ad341befe2b96dc561d7692e02ab39c7bef26c67f37a5f46e', 
+	'xchacha'
+);
+let decrypted = seasalt.decrypt(
+	ciphertext, 
+	'a3a44974a290878ad341befe2b96dc561d7692e02ab39c7bef26c67f37a5f46e', 
+	'xchacha'
+);
+```
+
+#### Password Hashing
+
+Supports Argon2 and SCrypt.
+
 ```js
 let seasalt = new SeaSalt;
+let password = 'test'
+let hash = seasalt.pwhash_create(password, 'normal', 'argon2');
+let verify = seasalt.pwhash_verify(hash, password);
+```
 
-let original = 'Text to encrypt';
-let ciphertext = seasalt.aead_encrypt(
-	original, 
-	'myPassphrase', 
-	'xchacha'
-);
+#### String Hashing
 
-let decrypted = seasalt.aead_decrypt(
-	ciphertext, 
-	'myPassphrase', 
-	'xchacha'
-);
+Supports SHA256 and SHA512.
+
+```js
+let seasate = new SeaSalt;
+let string = 'test'
+let hash = seasalt.sha256(string);
 ```
 
 ## Setting Defaults
@@ -28,7 +59,11 @@ let decrypted = seasalt.aead_decrypt(
 You can set the encryption key in advanced by passing it along at runtime:
 
 ```js
-let seasalt = new SeaSalt({passphrase: 'myPassphrase'});
+let seasalt = new SeaSalt({
+    algorithm: 'xchacha',
+    secret: 'changeme3xg4#',
+    pwhash: 'argon2'
+});
 ```
 
 ## Notes
--- src/seasalt.js
+++ src/seasalt.js
@@ -8,12 +8,13 @@ class SeaSalt {
 
         this.version = {
             major: 0,
-            minor: 1
+            minor: 2
         };
 
         this.config = {
             algorithm: 'xchacha',
-            passphrase: 'changeme3xg4#'
+            secret: 'changeme3xg4#',
+            pwhash: 'argon2'
         };
 
         this.state = {
@@ -21,8 +22,19 @@ class SeaSalt {
             aead: false
         };
 
+        //  it must be one of these
+
+        if ( ['argon2', 'scrypt'].indexOf(this.config.pwhash) === -1 ) this.config.pwhash = 'argon2';
+
+        //  link classes
+        this.pwhash = {};
+        this.pwhash.argon2 = new SeaSalt_PWHash_Argon2;
+        this.pwhash.scrypt = new SeaSalt_PWHash_SCrypt;
+
         this.aead = {};
         this.aead.xchacha = new SeaSalt_AEAD_XChaCha;
+
+        this.hash = new SeaSalt_Hashing;
 
         //  check if jquery is available
         if ( typeof $ === 'undefined' ) {
@@ -45,57 +57,62 @@ class SeaSalt {
 
     }
 
-    //  encrypt a string using an aead cipher
-    aead_encrypt(string, passphrase, algorithm) {
-
-        if ( this.state.ready === false ) {
-            console.error('SeaSalt.aead_encrypt() cannot execute if SeaSalt is not ready');
-            return;
-        }
+    //  encrypt the data with the requested algorithm
+    encrypt(string, secret, algorithm) {
 
         if ( !string ) {
-            console.error('SeaSalt.aead_encrypt() requires a string or object to encrypt');
+            console.error('SeaSalt.encrypt() requires a string or object to encrypt');
             return;
         }
 
         if ( !algorithm ) algorithm = this.config.algorithm;
-        if ( !passphrase ) passphrase = this.config.passphrase;
+        if ( !secret ) secret = this.config.secret;
         if ( typeof this.aead[algorithm] === 'object' ) {
 
-            return this.aead[algorithm].encrypt(string, passphrase);
+            return this.aead[algorithm].encrypt(string, secret);
 
         } else {
 
-            console.error('SeaSalt.aead_encrypt() received invalid algorithm - ' + algorithm);
+            console.error('SeaSalt.encrypt() received invalid algorithm - ' + algorithm);
 
         }
 
     }
 
-    //  decrypt the supplied aead ciphertext
-    aead_decrypt(string, passphrase, algorithm) {
-
-        if ( this.state.ready === false ) {
-            console.error('SeaSalt.aead_decrypt() cannot execute if SeaSalt is not ready');
-            return;
-        }
+    decrypt(string, secret, algorithm) {
 
         if ( !string ) {
-            console.error('SeaSalt.aead_decrypt() requires a string to decrypt');
+            console.error('SeaSalt.aead.decrypt() requires a string to decrypt');
             return;
         }
 
         if ( !algorithm ) algorithm = this.config.algorithm;
-        if ( !passphrase ) passphrase = this.config.passphrase;
+        if ( !secret ) secret = this.config.secret;
         if ( typeof this.aead[algorithm] === 'object' ) {
 
-            return this.aead[algorithm].decrypt(string, passphrase);
+            return this.aead[algorithm].decrypt(string, secret);
 
         } else {
 
-            console.error('SeaSalt.aead_decrypt() received invalid algorithm - ' + algorithm);
+            console.error('SeaSalt.aead.decrypt() received invalid algorithm - ' + algorithm);
 
         }
+
+    };
+
+    pwhash_create(password, security, algorithm) {
+
+        if ( !algorithm ) algorithm = this.config.pwhash;
+        if ( algorithm && Object.keys(this.pwhash).indexOf(algorithm) === -1 ) algorithm = this.config.pwhash;
+        return this.pwhash[algorithm].create(password, security);
+
+    }
+
+    pwhash_verify(hash, password, algorithm) {
+
+        if ( !algorithm ) algorithm = this.config.pwhash;
+        if ( algorithm && Object.keys(this.pwhash).indexOf(algorithm) === -1 ) algorithm = this.config.pwhash;
+        return this.pwhash[algorithm].verify(hash, password);
 
     }
 
@@ -111,56 +128,174 @@ class SeaSalt {
     aead_test() {
 
         let original = 'test';
-        let ciphertext = this.aead_encrypt(original);
-        let result = this.aead_decrypt(ciphertext);
+        let ciphertext = this.encrypt(original);
+        let result = this.decrypt(ciphertext);
         return ( original === result);
 
     }
 
 }
 
+/* Generic Hashing */
+
+class SeaSalt_Hashing {
+
+    sha256(string, format) {
+
+        if ( !format ) format = 'hex';
+        if ( ['ascii', 'hex', 'binary', 'base64'].indexOf(format) === -1 ) format = 'hex';
+        let result = sodium.crypto_hash_sha256(string);
+        if ( format === 'hex' ) return sodium.to_hex(result);
+        if ( format === 'base64' ) return sodium.to_base64(result);
+        return result;
+
+    }
+
+    sha512(string, format) {
+
+        if ( !format ) format = 'hex';
+        if ( ['ascii', 'hex', 'binary', 'base64'].indexOf(format) === -1 ) format = 'hex';
+        let result = sodium.crypto_hash_sha512(string);
+        if ( format === 'hex' ) return sodium.to_hex(result);
+        if ( format === 'base64' ) return sodium.to_base64(result);
+        return result;
+
+    }
+
+}
+
+/* PWHash Classes */
+
+class SeaSalt_PWHash_Argon2 {
+
+    /*
+     crypto_pwhash_OPSLIMIT_INTERACTIVE = 32Mb
+     crypto_pwhash_OPSLIMIT_MODERATE = 128Mb / 0.7s
+     crypto_pwhash_OPSLIMIT_SENSITIVE = 512Mb / 3.5s
+    */
+    create(password, security) {
+
+        let opsLimit = sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE;
+        let memLimit = sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE;
+
+        if ( !security ) security = 'normal';
+        if ( ['light', 'normal', 'moderate', 'high'].indexOf(security) === -1 ) security = 'normal';
+        if ( security === 'normal' ) {
+
+            opsLimit = opsLimit*2;
+
+        } else if ( security === 'moderate' ) {
+
+            opsLimit = opsLimit*4;
+
+        } else if ( security === 'high' ) {
+
+            opsLimit = opsLimit*6;
+
+        }
+
+        console.log(opsLimit, memLimit);
+        return sodium.crypto_pwhash_str(password, opsLimit, memLimit);
+
+    }
+
+    verify(hash, password) {
+
+        return sodium.crypto_pwhash_str_verify(hash, password);
+
+    }
+
+}
+
+class SeaSalt_PWHash_SCrypt {
+
+    /*
+     crypto_pwhash_OPSLIMIT_INTERACTIVE = 32Mb
+     crypto_pwhash_OPSLIMIT_MODERATE = 128Mb / 0.7s
+     crypto_pwhash_OPSLIMIT_SENSITIVE = 512Mb / 3.5s
+     */
+    create(password, security) {
+
+        let opsLimit = sodium.crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_INTERACTIVE;
+        let memLimit = sodium.crypto_pwhash_scryptsalsa208sha256_MEMLIMIT_INTERACTIVE;
+
+        if ( !security ) security = 'normal';
+        if ( ['light', 'normal', 'moderate', 'high'].indexOf(security) === -1 ) security = 'normal';
+        if ( security === 'normal' ) {
+
+            opsLimit = sodium.crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_INTERACTIVE*3;
+
+        } else if ( security === 'moderate' ) {
+
+            opsLimit = (sodium.crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_INTERACTIVE+sodium.crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_SENSITIVE)*0.25;
+
+        } else if ( security === 'high' ) {
+
+            opsLimit = sodium.crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_SENSITIVE;
+
+        }
+
+        console.log(opsLimit);
+        return sodium.crypto_pwhash_scryptsalsa208sha256_str(password, opsLimit, memLimit);
+
+    }
+
+    verify(hash, password) {
+
+        return sodium.crypto_pwhash_scryptsalsa208sha256_str_verify(hash, password);
+
+    }
+
+}
+
+/* AEAD Classes */
+
 //
 //  XChaCha20-Poly1305-IETF AEAD Methods
 //
 class SeaSalt_AEAD_XChaCha {
 
-    encrypt(string, passphrase) {
+    encrypt(string, secret) {
 
-        if ( !string || !passphrase ) {
-            console.error('SeaSalt.aead.xchacha.encrypt() requires a string or passphrase to encrypt');
+        if ( !string || !secret ) {
+            console.error('SeaSalt.aead.xchacha.encrypt() requires a string or secret to encrypt');
             return;
         }
 
         let nonce = sodium.randombytes_buf(sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES);
-        let key = sodium.from_hex($.sha256(passphrase));
+        let key = sodium.from_hex($.sha256(secret));
         let ciphertext = sodium.crypto_aead_xchacha20poly1305_ietf_encrypt(string, '', '', nonce, key);
+        sodium.memzero(key);
         return sodium.to_hex(nonce) + sodium.to_hex(ciphertext);
 
     };
 
-    decrypt(string, passphrase) {
+    decrypt(string, secret) {
 
         if ( !string ) {
-            console.error('SeaSalt.aead_decrypt() requires a string to decrypt');
+            console.error('SeaSalt.aead.decrypt() requires a string to decrypt');
             return;
         }
 
 
         let nonce = sodium.from_hex(string.substr(0, sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES*2));
         let ciphertext = sodium.from_hex(string.substr(sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES*2, string.length));
-        let key = sodium.from_hex($.sha256(passphrase));
+        let key = sodium.from_hex($.sha256(secret));
         let result = '';
         try {
             result = sodium.crypto_aead_xchacha20poly1305_ietf_decrypt('', ciphertext, '', nonce, key);
         } catch (e) {}
+        sodium.memzero(key);
         return ( result ) ? sodium.to_string(result) : undefined;
 
     };
 
     key() {
 
-        return sodium.to_hex(sodium.randombytes_buf(sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES));
+        return sodium.to_hex(sodium.crypto_aead_xchacha20poly1305_ietf_keygen());
 
     }
 
 }
+
+//  AES-256-GCM not supported in browser library
